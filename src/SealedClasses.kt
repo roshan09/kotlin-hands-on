1b5936018fda526ce3f19be81fa29f4d2f00327d
roshan <roshanbhor.09@gmail.com>
2020-06-07 11:10:45 +0530

    Roshan | Add sealed class

---
+++ src/SealedClasses.kt
@@ -0,0 +1,28 @@
+import Result.Success
+import Result.Error
+
+/*
+    Use sealed class to specify limited set of possibilities of classes
+    Alternative is enum
+    Problems with enum :
+        1) Enum classes only allow one instance of each type
+        2) Enum classes can't hold different info on different classes
+ */
+
+fun main() {
+    val result = findResult()
+    when (result) {
+        is Success -> println("Success result: " + result.data)
+        is Error -> print("Failure result: " + result.message + " error message: " + result.e.message)
+    }
+}
+
+fun findResult(): Result {
+    return Error("Failed to find result", Exception("error-123"));
+}
+
+
+sealed class Result {
+    data class Success(val data: String) : Result()
+    data class Error(val message: String, val e: Exception) : Result()
+}
